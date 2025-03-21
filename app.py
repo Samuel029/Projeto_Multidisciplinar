8572590887222b7c725a63e3c3a34d30c7244e95
Cxrzin08 <felipepereirabritto08@gmail.com>
2025-03-21 17:47:52 -0300

    test

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = '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'
+app.config['MAIL_PASSWORD'] = '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'
 app.config['MAIL_DEFAULT_SENDER'] = 'felipepereirabritto08@gmail.com'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
